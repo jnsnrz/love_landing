9bb7715c126a75e9a383259ece00818397b0c082
Yevgenia <nikolaeva.j@ukr.net>
2019-03-27 17:46:12 +0200

    Add files via upload

---
+++ loveforever/script.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', init, false);
+
+
+function init() {
+    bindDomEvents();
+}
+
+function bindDomEvents() {
+    $('.first-selection-button').on('click', function() {
+        hideAll();
+        $('.first-selection-button').addClass('active');
+        $('.first-selection-container').removeClass('hidden').addClass('visible');
+
+        $('html, body').animate({
+            scrollTop: $('.first-selection-container').offset().top
+        }, 900);
+    });
+
+    $('.second-selection-button').on('click', function() {
+        hideAll();
+        $('.second-selection-button').addClass('active');
+        $('.second-selection-container').removeClass('hidden').addClass('visible');
+
+        $('html, body').animate({
+            scrollTop: $('.second-selection-container').offset().top
+        }, 900);
+    });
+}
+function hideAll() {
+    $('.landing-love__selection-container').removeClass('visible').addClass('hidden');
+    $('.button__selection').removeClass('active');
+}
